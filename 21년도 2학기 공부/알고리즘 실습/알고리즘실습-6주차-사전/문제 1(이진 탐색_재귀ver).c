8d3c955766f03c3480306c807c9ff8cfa10ad7b1
symaeng98 <symaeng98@naver.com>
2021-10-09 17:59:25 +0900

    실습

---
+++ 21년도 2학기 공부/알고리즘 실습/알고리즘실습-6주차-사전/문제 1(이진 탐색_재귀ver).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int binarySearch(int *arr, int l, int r, int k);
+
+int main(){
+    int n, k, *arr,i;
+    scanf("%d",&n);
+    arr = (int*)malloc(sizeof(int)*n);
+    scanf("%d",&k);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf(" %d",binarySearch(arr,0,n-1,k));
+
+    return 0;
+}
+
+int binarySearch(int *arr, int l, int r, int k){
+    int m;
+    m = (l+r)/2;
+    if(l>=r){
+        if(k<arr[m]) return m-1;
+        else return m;
+    }
+    if(k == arr[m]) return m;
+    else if(k < arr[m]) return binarySearch(arr,l,m-1,k);
+    else return binarySearch(arr,m+1,r,k);
+}
